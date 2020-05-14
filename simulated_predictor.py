96c6492132fa810127b4208f7f2640ea5abe4696
user <typhoonrg@gmail.com>
2020-05-13 20:39:57 -0500

    Try to automate Kafka server start up

--- simulated_predictor.py
+++ simulated_predictor.py
@@ -10,19 +10,30 @@ from datetime import datetime, timedelta
 from fbprophet import Prophet
 
 import os
+import subprocess
+
 
 BROKER_URL = "PLAINTEXT://localhost:9092"
 TOPIC_NAME = "simulated-realtime-stock-predictor"
 STOCK = 'FB'
 api_key = 'B0N8Q38MJAVBSLOY'
+# Simulate stock market stream from yesterday
+DAY_TO_SIMULATE = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 
+async def start_zookeeper():
+    os.system("zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties")
+
+async def start_broker():
+    os.system("kafka-server-start /usr/local/etc/kafka/server.properties")
+
+async def start_kafka_server():
+    zookeeper = asyncio.create_task(start_zookeeper())
+    broker = asyncio.create_task(start_broker())
+    await zookeeper
+    await broker
 
 async def produce(topic_name):
-    """Produces data into the Kafka Topic"""
     p = Producer({"bootstrap.servers": BROKER_URL})
-
-    d = datetime.today() - timedelta(days=1)
-    day_to_simulate = d.strftime('%Y-%m-%d')
 
     ts = TimeSeries(key=api_key, output_format='pandas')
     data, meta_data = ts.get_intraday(symbol='FB', interval = '1min', outputsize = 'full')
@@ -37,13 +48,13 @@ async def produce(topic_name):
 
     # Only consider one day of data
     data_prophet['string_ds'] = data_prophet['ds'].astype(str)
-    data_prophet = data_prophet[data_prophet.string_ds.str.startswith((day_to_simulate))]
+    data_prophet = data_prophet[data_prophet.string_ds.str.startswith((DAY_TO_SIMULATE))]
     data_prophet = data_prophet.drop(['string_ds'], axis = 1)
     data_prophet = data_prophet.reset_index(drop=True)
 
     i = 0
     market_open = True
-    market_closing_time = day_to_simulate + " 16:00:00"
+    market_closing_time = DAY_TO_SIMULATE + " 16:00:00"
     while (market_open):
         current_time = data_prophet['ds'].iloc[i]
         current_price = data_prophet['y'].iloc[i]
@@ -60,12 +71,12 @@ async def produce(topic_name):
 
 
 async def consume(topic_name):
-    """Consumes data from the Kafka Topic"""
     c = Consumer(
         {"bootstrap.servers": BROKER_URL, "group.id": "my-first-consumer-group"}
     )
     c.subscribe([topic_name])
 
+    print("Streaming stock market data for " + STOCK + " on " + DAY_TO_SIMULATE)
     while True:
         message = c.poll(1.0)
         
@@ -79,27 +90,21 @@ async def consume(topic_name):
 
 
 async def produce_consume():
-    """Runs the Producer and Consumer tasks"""
     t1 = asyncio.create_task(produce(TOPIC_NAME))
     t2 = asyncio.create_task(consume(TOPIC_NAME))
     await t1
     await t2
 
-
 def main():
-    """Runs the exercise"""
     try:
-        # Start and set up Kafka Server
-        os.system('zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties')
-        os.system('kafka-server-start /usr/local/etc/kafka/server.properties')
-
-        client = AdminClient({"bootstrap.servers": BROKER_URL})
-        topic = NewTopic(TOPIC_NAME, num_partitions=1, replication_factor=1)
-        client.create_topics([topic])
+        asyncio.run(start_kafka_server())
     except KeyboardInterrupt as e:
         print("Did not finish setting up Kafka server")
-    finally:
-        client.delete_topics([topic])
+    
+    client = AdminClient({"bootstrap.servers": BROKER_URL})
+    topic = NewTopic(TOPIC_NAME, num_partitions=1, replication_factor=1)
+    client.create_topics([topic])
+    print("All set up!")
 
     try:
         asyncio.run(produce_consume())
